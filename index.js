b11e45569f9e25610e79956f604b4ffc4bf5acce
AmmarEldik <vektorea60@gmail.com>
2022-10-23 18:11:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+function changeText (){
+    const newText = document.getElementById("text").innerHTML = "This is really cool!";
+    return newText ;
+
+}
+
+document.addEventListener("DOMContentLoaded", changeText);
+  
